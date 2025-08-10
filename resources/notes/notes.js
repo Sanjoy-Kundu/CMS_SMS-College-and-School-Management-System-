889520dcc2fc98e4d09563cb06286368a649f074
unknown <sanjoykundu187@gmail.com>
2025-08-10 17:08:54 +0600

    Admin Controller Model migration just creating

---
+++ resources/notes/notes.js
@@ -0,0 +1,16 @@
+/*
+Step:1
+  i. First create users schema for login to check role 
+     Now Make another shema for Admin for login and others AdminController Model Schema
+
+
+
+
+
+
+
+
+
+
+
+*/
